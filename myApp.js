3d1b4bb838f9899b1052094757d69228090ebaaa
Antonio Gonzalez <Antonio.Gonzalez@avenuinsights.com>
2021-12-01 15:28:29 -0600

    adding bcrypt to server.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
-const helmet = require("helmet");
+//const helmet = require("helmet");
 
 module.exports = app;
 const api = require('./server.js');
 
-app.use(helmet());
+/*app.use(helmet(
+  {
+    frameguard: { action: "deny" },
+    contentSecurityPolicy:
+    {
+      directives:
+      {
+        defaultSrc: ["'self'"],
+        styleSrc: ['style.com']
+        //scriptSrc: ["'self'", 'trusted-cdn.com']
+      }
+    },
+    dnsPrefetchControl: false
+  }
+));
 app.use(helmet.hidePoweredBy());
+//app.use(helmet.frameguard({ action: "deny" }));
+app.use(helmet.frameguard());
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({
+  maxAge: 90*24*60*60,
+  force: true
+}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());*/
+/*app.use(helmet.contentSecurityPolicy({
+  directives:
+  {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));*/
+//app.use(helmet.contentSecurityPolicy());
 
 app.use(express.static('public'));
 app.disable('strict-transport-security');
